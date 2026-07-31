01dac4f885c09d999116e8756a090635ea7ff00b
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-31 18:53:54 +0545

    Testing the code rabbit

---
+++ backend/src/test.js
@@ -0,0 +1,11 @@
+function calculateTotal(items) {
+  let total = 0;
+
+  for (let i = 0; i < items.length; ++i) {
+    total = total + items[i].price;
+  }
+
+  console.log("total : ", total);
+
+  return total;
+}
