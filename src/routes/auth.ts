d65c69df1b3cba55117298ec5c031a128f4c5102
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-09-02 18:29:11 +0545

    feat : full wipe out

--- src/routes/auth.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerUser } from "../controllers/authControllers";
-
-const router = Router();
-
-router.post("/register", registerUser);
-
-export default router;
